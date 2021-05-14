a6bbf72b329dcd0bdefa42a43aa2e76093478a8e
fblz <fabu@fblz.de>
2021-05-14 02:07:49 +0200

    Added xfce terminal configuration

--- setup.sh
+++ setup.sh
@@ -30,9 +30,11 @@ for profile in "$@"; do
         mkdir -p ~/.config/i3/
         mkdir -p ~/.config/i3status/
         mkdir -p ~/.config/rofi/
+        mkdir -p ~/.config/xfce4/terminal/
         ln -fs $sources/.config/i3/config ~/.config/i3/config
         ln -fs $sources/.config/i3status/config ~/.config/i3status/config
         ln -fs $sources/.config/rofi/config.rasi ~/.config/rofi/config.rasi
+        ln -fs $sources/.config/xfce4/terminal/terminalrc ~/.config/xfce4/terminal/terminalrc
         ;;
     *)
         echo "unknown profile"
