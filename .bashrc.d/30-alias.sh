2dd190fceb25b480b805ee1613a8902fdbf1c447
fblz <fabu@fblz.de>
2021-07-13 14:38:40 +0200

    added yolossh

--- .bashrc.d/30-alias.sh
+++ .bashrc.d/30-alias.sh
@@ -1,3 +1,4 @@
 alias ls='ls --color=auto'
 alias ll='ls -FAhl'
 alias la='ls -FAh'
+alias yolossh='ssh -o GlobalKnownHostsFile=/dev/null -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=accept-new'
