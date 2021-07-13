3ef6a69e3428513d3e0827ea82e6978270546e6a
fblz <fabu@fblz.de>
2021-07-13 14:39:10 +0200

    added prerequisites installer

--- prerequisites.sh
+++ prerequisites.sh
@@ -33,6 +33,9 @@ for profile in "$@"; do
         fedora)
             dnf install --assumeyes tmux git vim fzf bash-completion
             ;;
+        opensuse-leap)
+            zypper install --no-confirm tmux git vim fzf bash-completion
+            ;;
         *)
             echo "not configured"
             ;;
@@ -43,7 +46,7 @@ for profile in "$@"; do
             dnf groupinstall --assumeyes i3
             rpm --import https://packages.microsoft.com/keys/microsoft.asc
             echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo
-            dnf install --assumeyes xfce4-terminal xss-lock rofi maim ImageMagick keepassxc nextcloud-client fontawesome-fonts code feh
+            dnf install --assumeyes xfce4-terminal xss-lock rofi maim ImageMagick keepassxc nextcloud-client fontawesome-fonts code feh remmina
             ;;
         *)
             echo "not configured"
