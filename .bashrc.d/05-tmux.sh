4962d5b4df85c069b22b2824a21e8c3c126ba715
fblz <fblz@users.noreply.github.com>
2025-01-04 20:53:09 +0100

    Somwhere along the way the interactivity check got lost. Added that and some comments.

--- .bashrc.d/05-tmux.sh
+++ .bashrc.d/05-tmux.sh
@@ -1,14 +1,23 @@
 # leave as 05-*, this will exec into tmux so we don't need other bash config
+
+# Exit if non interactive
+[ -z "$PS1" ] && return
+
+# Exit if not in an SSH session
 [ -z "$SSH_TTY" ] && return
+
+# Check if tmux is installed
 command -v "tmux" >/dev/null || return
 
 if [ -n "$TMUX" ]; then
+  # if already inside tmux, add this function and return
   function update-agent {
     export $(tmux show-environment | grep "^SSH_AUTH_SOCK=") > /dev/null
   }
   return
 fi
 
+# This is to skip the auto tmux. Use with /etc/ssh/sshd_config.d/30-autotmux.conf
 [ -n "$SKIPTMUX" ] && return
 
 if sudo -nv 2>/dev/null; then
