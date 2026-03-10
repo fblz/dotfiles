fd519513d03a58099e2e09c679ab63468c8d0c20
fblz <fblz@users.noreply.github.com>
2026-03-10 03:20:45 +0000

    fixed tmux execution order and agent forwarding

--- .bashrc.d/05-tmux.sh
+++ .bashrc.d/05-tmux.sh
@@ -3,9 +3,6 @@
 # Exit if non interactive
 [ -z "$PS1" ] && return
 
-# Exit if not in an SSH session
-[ -z "$SSH_TTY" ] && return
-
 # Check if tmux is installed
 command -v "tmux" >/dev/null || return
 
@@ -17,12 +14,15 @@ if [ -n "$TMUX" ]; then
   return
 fi
 
+# Exit if not in an SSH session
+[ -z "$SSH_TTY" ] && return
+
 # This is to skip the auto tmux. Use with /etc/ssh/sshd_config.d/30-autotmux.conf
 [ -n "$SKIPTMUX" ] && return
 
 if sudo -nv 2>/dev/null; then
   # If we have sudo, we sudo into tmux.
-	exec sudo tmux new-session -A -s ${TMUXSESSION:=ssh} -c '~'
+	exec sudo --preserve-env=SSH_AUTH_SOCK tmux new-session -A -s ${TMUXSESSION:=ssh} -c '~'
 fi
 
 # Else we run as the user
